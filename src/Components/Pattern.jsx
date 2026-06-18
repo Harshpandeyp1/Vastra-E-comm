e29a38b9617ce290c29f548748a7132601b5e282
Harsh pandey <hk9976057@gmail.com>
2026-06-18 20:50:25 +0530

    Delete src directory

--- src/Components/Pattern.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Pattern = () => {
-  return (
-    <div className="absolute inset-0 z-0 bg-[#313131] bg-[radial-gradient(rgba(255,255,255,0.15)_2px,transparent_0)] [background-size:30px_30px] bg-[position:-5px_-5px]" />
-  )
-}
-
-export default Pattern
