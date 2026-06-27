1a15d702188608473b7bea03c7446d80bdcf18e7
Harsh pandey <hk9976057@gmail.com>
2026-06-27 22:59:30 +0530

    Add files via upload

---
+++ backend e comm/src/main/java/com/Ecomm/prj/Dto/ChatRequest.java
@@ -0,0 +1,19 @@
+package com.Ecomm.prj.Dto;
+
+public class ChatRequest {
+
+    private String message;
+
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+
+
+
+}
