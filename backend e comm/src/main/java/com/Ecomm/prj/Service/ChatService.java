ae9950f3829a9721931354c5f84061ac0f38d7da
Harsh pandey <hk9976057@gmail.com>
2026-06-28 21:43:39 +0530

    Add files via upload

--- backend e comm/src/main/java/com/Ecomm/prj/Service/ChatService.java
+++ backend e comm/src/main/java/com/Ecomm/prj/Service/ChatService.java
@@ -3,19 +3,27 @@ package com.Ecomm.prj.Service;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 @Service
 public class ChatService {
-    public final ChatClient chatClient;
-    public ChatService(ChatClient.Builder builder){
-        this.chatClient=builder.build();
+
+    private final ChatClient chatClient;
+
+    public ChatService(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
     }
-    public String getResponse(String message){
-        return chatClient
+
+    public String getResponse(String message) {
+
+        System.out.println("Calling Gemini...");
+
+        String response = chatClient
                 .prompt()
                 .user(message)
                 .call()
                 .content();
-    }
 
+        System.out.println("Gemini responded.");
+
+        return response;
+    }
 }
