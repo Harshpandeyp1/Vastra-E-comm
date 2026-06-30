80d589a4192c5347ca29ccdc036a3115e32def31
Harsh pandey <hk9976057@gmail.com>
2026-06-30 18:11:14 +0530

    Add files via upload

--- E comm/src/App.jsx
+++ E comm/src/App.jsx
@@ -1,21 +1,36 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
+import { useEffect } from "react"
 import Landing from "./Pages/Landing"
 import Login from "./Pages/Login"
 import Mainpage from "./Pages/Mainpage"
 import Men from "./Pages/Men"
- import Women from "./Pages/Women"
+import Women from "./Pages/Women"
 import Kids from "./Pages/Kids"
 import Trendings from "./Pages/Trending"
 import Outlet from "./Pages/Outlet"
 import Cart from "./Pages/Cart"
 import Order from "./Pages/Order"
- import Profile from "./Pages/Profile"
+import Profile from "./Pages/Profile"
 import Wishlist from "./Pages/Wishlist"
 import Searchpage from "./Components/Searchpage"
 import Collections from "./Pages/Collections"
+import Checkout from "./Pages/Checkout"
+import Success from "./Pages/Success"
+
+function ScrollToTop(){
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Landing />} />
           <Route path="/collections" element={<Collections />} />
@@ -31,6 +46,8 @@ function App() {
        {<Route path="/Profile" element={<Profile />} /> }
        <Route path="/Order" element={<Order />} />
        <Route path="/Wishlist" element={<Wishlist />} />
+       <Route path="/checkout" element={<Checkout />} />
+       <Route path="/success" element={<Success />} />
   
       </Routes>
     </BrowserRouter>
