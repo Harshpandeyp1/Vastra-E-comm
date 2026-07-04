06444822bee0bbba2e6f458b87b8a5c1401a30a8
Harsh pandey <hk9976057@gmail.com>
2026-07-04 22:04:44 +0530

    Add files via upload

--- backend e comm/src/main/java/com/Ecomm/prj/Dto/LoginResponseDto.java
+++ backend e comm/src/main/java/com/Ecomm/prj/Dto/LoginResponseDto.java
@@ -5,6 +5,7 @@ public class LoginResponseDto {
     private Long id;
     private String username;
     private String email;
+    private String token;
 
     public LoginResponseDto() {
     }
@@ -13,6 +14,13 @@ public class LoginResponseDto {
         this.id = id;
         this.username = username;
         this.email = email;
+    }
+
+    public LoginResponseDto(Long id, String username, String email, String token) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.token = token;
     }
 
     public Long getId() {
@@ -37,5 +45,13 @@ public class LoginResponseDto {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
     }
 }
