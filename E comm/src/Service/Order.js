ec277068f5310ed242dfdc41b935fc587d5fc8b8
Harsh pandey <hk9976057@gmail.com>
2026-07-08 21:25:30 +0530

    Add files via upload

--- E comm/src/Service/Order.js
+++ E comm/src/Service/Order.js
@@ -4,24 +4,53 @@ import axios from 'axios'
 
 const API ='http://localhost:8081/order'
 
-export const placeOrder=async(orderData)=>{
-  try{
-    console.log('sending order to',API,orderData);
+export const placeOrder = async (orderData) => {
+  try {
+    const token = localStorage.getItem("token");
 
-    const resp=await axios.post(`${API}/place`,orderData,{
-      headers:{'Content-Type':'application/json'},
-    });
+    console.log("Token:", token);
+    console.log("Sending order:", orderData);
+
+    if (!token) {
+      throw new Error("No token found");
+    }
+
+    const resp = await axios.post(
+      `${API}/place`,
+      orderData,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
     return resp.data;
-  }catch(err){
-    if(err?.response){
-    console.error('order API error',err.response.status,err.response.data);
-  }else{
-     console.error('order api request failed',err.message||err);
+  } catch (err) {
+    if (err.response) {
+      console.error(
+        "Order API error",
+        err.response.status,
+        err.response.data
+      );
+    } else {
+      console.error(err.message);
+    }
+    throw err;
   }
-  throw err;
-}
-}
-export const getUserOrders=async(userId)=>{
-  const res=await axios.get(`http://localhost:8081/order/user/${userId}`);
+};
+export const getUserOrders = async (userId) => {
+  const token = localStorage.getItem("token");
+
+  const res = await axios.get(
+    `${API}/user/${userId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
   return res.data;
-}
+};
