9e23dcf6fa9b310ea1fe1e6015e89a3a8c16075c
Harsh pandey <hk9976057@gmail.com>
2026-07-09 22:21:34 +0530

    Add files via upload

--- backend e comm/src/main/java/com/Ecomm/prj/repository/orderrepo.java
+++ backend e comm/src/main/java/com/Ecomm/prj/repository/orderrepo.java
@@ -4,8 +4,9 @@ import com.Ecomm.prj.Model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface orderrepo extends JpaRepository<Order,Integer> {
-
-
+    List<Order> findByUser_Id(Long userId);
 }
