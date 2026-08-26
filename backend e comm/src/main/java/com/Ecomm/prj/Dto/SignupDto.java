331224baff0fc45ee11feac10a1ee82f2e0938fb
Harsh pandey <hk9976057@gmail.com>
2026-08-26 23:22:49 +0530

    Add files via upload

--- backend e comm/src/main/java/com/Ecomm/prj/Dto/SignupDto.java
+++ backend e comm/src/main/java/com/Ecomm/prj/Dto/SignupDto.java
@@ -1,27 +1,25 @@
 package com.Ecomm.prj.Dto;
 
+import com.Ecomm.prj.Model.Role;
+import lombok.Getter;
+
+@Getter
 public class SignupDto {
     private String email;
     private String username;
 
-    public String getPassword() {
-        return password;
+    public void setRole(Role role) {
+        this.role = role;
     }
+
+    private Role role;
 
     public void setPassword(String password) {
         this.password = password;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
     }
 
     public void setEmail(String email) {
